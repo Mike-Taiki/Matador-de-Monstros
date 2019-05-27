1ea04447d40a7a6cdc3d92f5ecb299e1a351b2fe
Michael Gomes <michael.21.2012@hotmail.com>
2019-05-27 01:01:14 -0300

    Implementando lógica de exibição de dano

--- app.js
+++ app.js
@@ -5,12 +5,34 @@ new Vue({
     start: false,
     ataqueMonstro: 0,
     ataqueJogador: 0,
-    porcentagem: '100'
+    vidaJogador: '100',
+    vidaMonstro: '100',
+    perdeu: false,
+    dano: false,
+    danos: {
+      jogador: [],
+      monstro: []
+    }
   },
   methods: {
     ataque() {
-      this.ataqueMonstro = Math.floor(Math.random() * 20);
-      this.ataqueJogador = Math.floor(this.ataqueMonstro/3) * 2;
+      this.ataqueMonstro = Math.floor(Math.random() * 20)
+      this.ataqueJogador = Math.floor(this.ataqueMonstro/3) * 2
+    
+      this.vidaJogador -= this.ataqueMonstro
+      this.vidaMonstro -= this.ataqueJogador
+
+      this.dano = !this.dano;
+
+      this.danos.jogador.push(this.ataqueMonstro);
+      this.danos.monstro.push(this.ataqueJogador);
+
+      console.log(this.danos);
+
+      if(this.vidaJogador <= 0) {
+        this.vidaJogador = '0'
+        this.perdeu = true
+      }
     }
   }
-})
+});
